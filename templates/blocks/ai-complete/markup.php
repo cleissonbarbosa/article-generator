b6a67c0fe7b1ab4f3d7deb155c1b9d5dfe308e3e
cleissonbarbosa <cleisson147@gmail.com>
2024-06-30 11:06:33 -0300

    fix: compose deps

--- templates/blocks/ai-complete/markup.php
+++ templates/blocks/ai-complete/markup.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Block markup
  *
@@ -8,18 +9,18 @@
  * @var array    $context            Block context.
  */
 ?>
-    <?php if( is_user_logged_in() && current_user_can( 'edit_posts' )) : ?>
-        <div class="wp-block-article-generator-subject">
-            <div class="flex justify-between mb-8">
-                <small><?php _e('Article Generator', 'article-gen'); ?></small>
-                <small><?php _e('Don\'t worry, only you are seeing this.', 'article-gen')?></small>
-            </div>
-            <p><?php _e('You have not generated the post content with the article generator block, click on edit and click on generate for the text to be added to the post', 'article-gen'); ?></p>
-            <?php if($attributes['prompt'] || $attributes['context']): ?>
-                <ul class="mt-5 text-sm">
-                    <li><?php printf(__('Your prompt: %s', 'article-gen'), $attributes['prompt'] ?? 'empty');?></li>
-                    <li><?php printf(__('Your context: %s', 'article-gen'), $attributes['context']) ?? 'empty';?></li>
-                </ul>
-            <?php endif; ?>
+<?php if (is_user_logged_in() && current_user_can('edit_posts')) : ?>
+    <div class="wp-block-article-generator-subject">
+        <div class="flex justify-between mb-8">
+            <small><?php _e('Article Generator', 'article-gen'); ?></small>
+            <small><?php _e('Don\'t worry, only you are seeing this.', 'article-gen') ?></small>
         </div>
-    <?php endif;?>
+        <p><?php _e('You have not generated the post content with the article generator block, click on edit and click on generate for the text to be added to the post', 'article-gen'); ?></p>
+        <?php if ($attributes['prompt'] || $attributes['context']) : ?>
+            <ul class="mt-5 text-sm">
+                <li><?php printf(__('Your prompt: %s', 'article-gen'), $attributes['prompt'] ?? 'empty'); ?></li>
+                <li><?php printf(__('Your context: %s', 'article-gen'), $attributes['context']) ?? 'empty'; ?></li>
+            </ul>
+        <?php endif; ?>
+    </div>
+<?php endif; ?>
